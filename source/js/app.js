82ad064335d51519861dec4f8726817ec81c19e6
Яньшин Сергей Сергеевич <yanshin.s@medindex.ru>
2021-08-29 23:06:14 +0300

    feat(4.9): add responsive form

--- source/js/app.js
+++ source/js/app.js
@@ -1,8 +1,28 @@
-const toggle = document.querySelector('.main-nav__toggle');
-const menu = document.querySelector('.main-nav__list');
+(function() {
+  const toggle = document.querySelector('.main-nav__toggle');
+  const menu = document.querySelector('.main-nav__list');
 
-const handleToggleClick = () => {
-  menu.classList.toggle('main-nav__list--visible')
-};
+  const handleToggleClick = () => {
+    menu.classList.toggle('main-nav__list--visible')
+  };
 
-toggle.addEventListener('click', handleToggleClick);
+  toggle.addEventListener('click', handleToggleClick);
+})();
+
+(function() {
+  const modal = document.querySelector('.modal');
+  const orderButton = document.querySelector('.promo-product__action');
+  const submitButton = document.querySelector('.cart-form__submit');
+
+  const orderButtonClickHandler = () => {
+    modal.classList.add('modal--open');
+  }
+
+  const submitButtonClickHandler = (evt) => {
+    evt.preventDefault();
+    modal.classList.remove('modal--open');
+  }
+
+  orderButton.addEventListener('click', orderButtonClickHandler);
+  submitButton.addEventListener('click', submitButtonClickHandler);
+})();
